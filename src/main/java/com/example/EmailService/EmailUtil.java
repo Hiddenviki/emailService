e3d19975764d8171c07fd57e46ecff3eac80331d
Victorya <87774625+Hiddenviki@users.noreply.github.com>
2023-10-14 23:08:40 +0300

    Update EmailUtil.java
    Update EmailUtil.java

--- src/main/java/com/example/EmailService/EmailUtil.java
+++ src/main/java/com/example/EmailService/EmailUtil.java
@@ -10,17 +10,15 @@ public class EmailUtil {
 
     public EmailUtil(String smtpHost, String smtpUser, String smtpPassword) {
     }
-
     private static Properties loadProperties() throws IOException {
         Properties properties = new Properties();
         properties.load(EmailUtil.class.getClassLoader().getResourceAsStream("config.properties"));
         return properties;
     }
 
-
     public static void sendEmail(String to, String subject, String text)
             throws MessagingException, IOException {
-        System.out.println("приплыли в sendEmail в EmailUtil: "+to+" "+subject+" "+text);
+        System.out.println("приплыли в sendEmail в EmailUtil: " + to + " " + subject + " " + text);
         Properties properties = loadProperties();
         Session mailSession = Session.getDefaultInstance(properties);
 
@@ -31,7 +29,7 @@ public class EmailUtil {
         message.setText(text);
 
         Transport tr = mailSession.getTransport();
-        tr.connect(properties.getProperty("mail.smtp.host"), Integer.parseInt(properties.getProperty("mail.smtp.port")), properties.getProperty("mail.smtp.user"),properties.getProperty("mail.smtp.password"));
+        tr.connect(properties.getProperty("mail.smtp.host"), Integer.parseInt(properties.getProperty("mail.smtp.port")), properties.getProperty("mail.smtp.user"), properties.getProperty("mail.smtp.password"));
 
         tr.sendMessage(message, message.getAllRecipients());
         tr.close();
