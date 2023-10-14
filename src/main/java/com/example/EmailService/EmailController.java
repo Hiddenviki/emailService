18e9736cbe8b2f49975d9aec91a267b43aa919cb
Victorya <87774625+Hiddenviki@users.noreply.github.com>
2023-10-14 23:07:41 +0300

    Update EmailController.java
    Update EmailController.java

--- src/main/java/com/example/EmailService/EmailController.java
+++ src/main/java/com/example/EmailService/EmailController.java
@@ -1,31 +1,34 @@
-package com.example.EmailService;
+//package com.example.EmailService;
 
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-//оно тут вообще походу не надо
-public class EmailController {
+////import org.springframework.mail.SimpleMailMessage;
+////import org.springframework.mail.javamail.JavaMailSender;
 //
-//    private final EmailService emailService;
+//import org.springframework.web.bind.annotation.*;
 //
-//    public EmailController(JavaMailSender mailSender, EmailService emailService) {
-//        this.emailService = emailService;
+//@RestController
+////оно тут вообще походу не надо
+//public class EmailController {
+////
+////    private final EmailService emailService;
+////
+////    public EmailController(JavaMailSender mailSender, EmailService emailService) {
+////        this.emailService = emailService;
+////    }
+//
+//    @GetMapping("/")
+//    public String emailPage() {
+//        return "email";
 //    }
-
-    @GetMapping("/")
-    public String emailPage(){return "email";}
-
-//    @PostMapping("/sendEmail")
-//    public void sendEmail(@RequestParam("email") String email,
-//                          @RequestParam("body") String body) {
 //
-//        emailMessage.setTo(email);
-//        emailMessage.setSubject("no-reply");
-//        emailMessage.setText(body);
-//
-//        emailService.sendEmail(emailMessage);
-//
-//    }
-}
+////    @PostMapping("/sendEmail")
+////    public void sendEmail(@RequestParam("email") String email,
+////                          @RequestParam("body") String body) {
+////
+////        emailMessage.setTo(email);
+////        emailMessage.setSubject("no-reply");
+////        emailMessage.setText(body);
+////
+////        emailService.sendEmail(emailMessage);
+////
+////    }
+//}
