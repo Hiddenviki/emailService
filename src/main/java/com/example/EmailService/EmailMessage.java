b4c4acd4fc7e280419853272f7801af6aaef8c22
Victorya <87774625+Hiddenviki@users.noreply.github.com>
2023-10-14 23:07:56 +0300

    Update EmailMessage.java
    Update EmailMessage.java

--- src/main/java/com/example/EmailService/EmailMessage.java
+++ src/main/java/com/example/EmailService/EmailMessage.java
@@ -4,16 +4,15 @@ import lombok.Data;
 
 @Data
 public class EmailMessage {
-
     private String email; //куда посылаем
     private String subject; //тема сообщения
     private String body; //текст сообщения
 
     public EmailMessage(String email, String subject, String body) {
-        this.email=email;
-        this.subject=subject;
-        this.body=body;
-    };
+        this.email = email;
+        this.subject = subject;
+        this.body = body;
+    }
 
 
 }
